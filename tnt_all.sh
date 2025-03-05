da09e471d580eae77cb249531f1537b0d06ff97b
nburgdorfer <nburgdorfer@gmail.com>
2025-03-04 21:09:06 -0500

    updates

--- tnt_all.sh
+++ tnt_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./tnt_sparse_depth.sh /mnt/Drive2/TNT/training Barn
+#./tnt_sparse_depth.sh /mnt/Drive2/TNT/training Barn
 #./tnt_sparse_depth.sh /mnt/Drive2/TNT/training Caterpillar
-./tnt_sparse_depth.sh /mnt/Drive2/TNT/training Church
+#./tnt_sparse_depth.sh /mnt/Drive2/TNT/training Church
 ./tnt_sparse_depth.sh /mnt/Drive2/TNT/training Courthouse
 #./tnt_sparse_depth.sh /mnt/Drive2/TNT/training Ignatius
 ./tnt_sparse_depth.sh /mnt/Drive2/TNT/training Meetingroom
